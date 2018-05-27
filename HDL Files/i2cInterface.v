0865858f50d403b63702e55365c622069f46f52a
AlexanderKnapik <s3543757@student.rmit.edu.au>
2018-05-27 15:04:36 +1000

    Initialising base Quartus Project Folder. Adding i2c work done in lab 2 weeks ago

--- HDL Files/i2cInterface.v
+++ HDL Files/i2cInterface.v
@@ -3,34 +3,93 @@
 //under the GNU General Public License v3.0 or any later version
 //05.04.2018
 
+/*
+    This file is part of FPGA-HDMI-Image-Overlay. 
+	https://github.com/AlexanderKnapik/FPGA-HDMI-Image-Overlay
+
+    FPGA-HDMI-Image-Overlay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FPGA-HDMI-Image-Overlay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FPGA-HDMI-Image-Overlay.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 module i2cInterface 
-
 //Parameters used for passing variables
-
 #( 
+	/*
 	input parameter [7:0] data			= 8'hFF,	//Data used for communcations
 	input parameter [7:0] slaveAddress	= 8'hFF,	//Address used for the I2C slave
 	input parameter [7:0] dataAddress	= 8'hFF 	//Address used for writing to a particular register
+	*/
+	
+	//24 Bit data used for storing the following:
+	//[23:16] 	Slave Address
+	//[15:8]	Register Address
+	//[7:0]		Data to be written to register. 
+	parameter [23:0] dataIn		= 24'hFFFFFF;	
+	
+	//Parameters used for calculating the values needed for creating the
+	//state machine clock. Values are in Hz.
+	parameter inputSpeed		= 5 	* ( 10 ** 6 ); 	//Input reference clock. 50MHz.
+	parameter sclSpeed			= 400 	* ( 10 ** 3 );	//SCL communication speed. 400kHz.
 )
 
 (
-	input i2cStart,		//Input to start communications
+	input clock50M,		//Input reference clock of 50MHz.
+	input i2cStart,		//Input signal to start communications
 	input reset_n,		//Active low reset
-	input clockIn,		//Input clock to the controller. Double the speed of SCL.
-	inout reg sda,		//I2C SDA
+	inout sda,			//I2C SDA
 	output reg scl		//I2C input communications reference clock. 400kHz
 );
 
-	localparam bitLength	= 8;		//Local parameter holding the bit length of communications.
+//==============================================================================
+//				CLOCK GENERATOR USED FOR THE STATE MACHINE CLOCK
+	
+	//Set the state machine to run at double the speed of SCL.
+	parameter stateSpeed = ( sclSpeed * 2 );
+	
+	//Define the bit width based on the clock speeds. .
+	//log2( inputSpeed / stateSpeed ),				Round up.
+	//ln( inputSpeed / stateSpeed ) / ln( 2 ),		Round up.
+	parameter clockWidth = 3;
+	
+	//Wire used for connecting the state clock to the state machine.
+	wire stateClock;
+	
+	clockGen 
+	//Pass the parameters to the instantiated module.
+	#( inputSpeed, stateSpeed, clockWidth )
+		clockDivider
+		(
+			.clockIn	( clock50M ),
+			.clockOut	( stateClock )
+		);
+		
+//==============================================================================
+
+
+	localparam bitLength	= 8;	//Local parameter holding the bit length of communications.
 	
 	reg currentState;
 	reg nextState;
-	reg ackOK;						//Register for state machine telling if the acknowledge bit is correct.
+	reg ack;						//Register for state machine telling if the acknowledge bit is correct.
 	reg endOK;						//Register for state machine telling if the i2c communcation is correct.
 	reg counterValue	= 0;		//Counter for storing the 
 	reg bytes 			= 2'b11;	//3 bytes. 1st is slave address, 2nd is register address, 3rd is register data.
 	reg writeData;					//Value holding the address and data to be written. 
 	
+	reg i2cSDAOut;					//Tri-state control for SDA Pin. (output = 1).
+	reg SDAOut;						//Internal module output bit for SDA. 
+	
+	assign sda = i2cSDAOut ? SDAOut : 1'bz;	//Tri state control for the i2c sda pin.
 	
 	localparam idleState		= 0;
 	localparam idleWait			= 1;
@@ -40,6 +99,7 @@ module i2cInterface
 	localparam clockHighState	= 1;
 	localparam clockLowState	= 1;
 	localparam acknowledgeState	= 1;
+	localparam stopState		= 1;
 	
 		
 	//Logic to handle the currentState -> nextState transition.
@@ -114,7 +174,7 @@ module i2cInterface
 					clockLowState:
 						begin
 							//If the counter is zero, read an acknowledge bit
-							if( counterValue == 1'b0 ) 
+							if( counterValue == 'd8 ) 
 								begin
 									nextState = acknowledgeState;
 								end
@@ -127,8 +187,17 @@ module i2cInterface
 					
 					acknowledgeState:
 						begin
-							//If SDA is low
-							if (
+							//Release SDA, and if the acknowledge register is low,
+							//That means the slave has acknowledged the communication
+							if ( ack == 1'b0 )
+								begin
+									//nextState = 
+								end
+							//Else stop communication, and go back to the stop state.
+							else	
+								begin
+									nextState = stopState;
+								end
 						end
 					
 					
@@ -145,7 +214,7 @@ module i2cInterface
 						//Idle, keep both SDA and SCL high.
 						sda			<= 1'b1;
 						scl			<= 1'b1;
-						ackOK		<= 1'b0;
+						ack			<= 1'b0;
 						endOK		<= 1'b0;
 						writeData	<= 0;
 						
@@ -156,7 +225,7 @@ module i2cInterface
 						//Idle, keep both SDA and SCL high. Waiting for i2cStart to go low. 
 						sda			<= 1'b1;
 						scl			<= 1'b1;
-						ackOK		<= 1'b0;
+						ack			<= 1'b0;
 						endOK		<= 1'b0;
 						writeData 	<= 0;
 					end
@@ -166,7 +235,7 @@ module i2cInterface
 						//Set SDA low while SCL is high to signalise the start of i2c communications.
 						sda			<= 1'b0;
 						scl			<= 1'b1;
-						ackOK		<= 1'b0;
+						ack			<= 1'b0;
 						endOK		<= 1'b0;
 						writeData 	<= slaveAddress & 1'b0;	//Set to write always.
 					end
